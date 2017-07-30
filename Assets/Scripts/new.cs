496d940da0960a0afe0446d576d446cd16f2cc62
SILVERFOX\Olivier <olivier.Dehecq@gmail.com>
2017-07-30 20:56:13 +0200

    master initial

---
+++ Assets/Scripts/new.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class new : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+    Debug.Log("Test");
+    Debug.Log("Ajout à la suite pour tester");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
